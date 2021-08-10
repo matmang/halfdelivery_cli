63b607f5049a0d8a0623476f22c4a8cbde2d7ae5
TomTimmy <70505699+TomTimmy@users.noreply.github.com>
2021-08-11 00:21:19 +0900

    iOS Build Failed 해결중.v1

---
+++ ios/halfdelivery-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  halfdelivery-Bridging-Header.h
+//  halfdelivery
+//
+//  Created by Ted Choi on 2021/08/10.
+//
+
+#ifndef halfdelivery_Bridging_Header_h
+#define halfdelivery_Bridging_Header_h
+
+
+#endif /* halfdelivery_Bridging_Header_h */
